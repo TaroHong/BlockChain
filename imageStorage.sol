430536044473c30f6a5717d5c466b64b39c8ad8b
Hong GiHyeok <121781376+TaroHong@users.noreply.github.com>
2024-12-06 16:02:41 +0900

    Update imageStorage.sol

--- imageStorage.sol
+++ imageStorage.sol
@@ -12,20 +12,19 @@ contract ImageStorage {
     BlockData[] public blocks;
 
     // 이벤트 추가
-    event BlockStored(uint256 index, uint256 timestamp, string inputDataHash, string previousHash, string result);
+    event BlockStored(uint256 index, uint256 timestamp, string inputDataHash, string previousHash);
 
     function storeBlock(
         uint256 index,
         uint256 timestamp,
         string memory inputDataHash,
-        string memory previousHash,
-        string memory result
+        string memory previousHash
     ) public {
         // 블록 저장
-        blocks.push(BlockData(index, timestamp, inputDataHash, previousHash, result));
+        blocks.push(BlockData(index, timestamp, inputDataHash, previousHash));
         
         // 블록 저장 이벤트 발생
-        emit BlockStored(index, timestamp, inputDataHash, previousHash, result);
+        emit BlockStored(index, timestamp, inputDataHash, previousHash);
     }
 
     function getBlock(uint256 index) public view returns (BlockData memory) {
